122fe7b98fdc8d32984dcb18c952b9d85dd4fe04
0Kaneca0 <viana.adryan@escola.pr.gov.br>
2025-05-29 08:49:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programação',
+    'Qual é o comando usado em Python para exibir uma mensagem na tela?'
+    'O comando é print()'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do planeta?',
+    'O maior oceano do planeta é o Oceano Pacífico.
+    'Ele cobre cerca de 1/3 da superfície da Terra e é também o mais profundo, com a Fossa das Marianas chegando a aproximadamente 11.000 metros de profundidade.'
+)
+
+criaCartao(
+    'Programação',
+    'Em qual linguagem de programação usamos o comando console.log() para exibir uma mensagem na tela?',
+    'Usamos o console.log() na linguagem JavaScript.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Onze de setembro de dois mil e um em Inglês?',
+    'September eleventh, two thousand and one'
+)               
